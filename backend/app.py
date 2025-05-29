e0b4646c4c432ffc5daf5d6cea07b6225aa46778
darmawan suhara <dsuharaaa@gmail.com>
2025-05-29 20:03:02 +0700

    fix app.py

--- backend/app.py
+++ backend/app.py
@@ -141,5 +141,7 @@ def predict_disease():
     except Exception as e:
         return jsonify({'error': str(e)}), 400
 
-if __name__ == "__main__":
-    app.run()
+
+if __name__ == '__main__':
+    port = int(os.environ.get('PORT', 5000))
+    app.run(host='0.0.0.0', port=port)
